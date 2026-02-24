cf912fc95c429140730f242400cd15de7198e00a
Rustambek <rusik.khanov06@gmail.com>
2026-02-25 02:17:34 +0500

    SecurityConfig

--- tracker/src/main/java/com/tracker/Config/SecurityConfig.java
+++ tracker/src/main/java/com/tracker/Config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
@@ -15,8 +17,16 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())          // иначе POST может ещё и по CSRF блочиться
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                //.requestMatchers("/**").denyAll()
                 .anyRequest().permitAll()
             );
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
